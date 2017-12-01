f45cc569c36784771398885471d2820e512be6e9
Christoph Schleßelmann <cschlesselmann@trustbills.com>
2017-12-01 08:45:04 +0100

    shortened

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,14 +1,11 @@
 file = open("input.txt")
 
 input = file.readline()
-input += input[0]
 
 sum=0
-for index in range(len(input) - 1):
-    if (input[index] == input[index + 1]):
+for index in range(len(input)):
+    compareIndex = (index + 1) % (len(input) - 1)
+    if (input[index] == input[compareIndex]):
         sum += int(input[index])
-
-if (input[0] == input[len(input) - 1]):
-    sum += int(input[0])
 
 print(sum)
