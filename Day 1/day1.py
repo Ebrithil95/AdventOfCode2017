e9b3ff4b34affb66e0224fe37032f5e14522dcec
Christoph Schleßelmann <cschlesselmann@trustbills.com>
2017-12-01 08:46:48 +0100

    Day 1 finished

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -9,3 +9,11 @@ for index in range(len(input)):
         sum += int(input[index])
 
 print(sum)
+
+sum=0
+for index in range(len(input)):
+    compareIndex = (index + int(len(input) / 2)) % (len(input) - 1)
+    if (input[index] == input[compareIndex]):
+        sum += int(input[index])
+
+print(sum)
