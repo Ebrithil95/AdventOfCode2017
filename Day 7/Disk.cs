4537ca8c44acc4f6483822a7669f39063debb979
Christoph Schleßelmann <c.schlesselmann@googlemail.com>
2017-12-19 00:03:37 +0100

    Day 7 Part 1 Done

--- Day 7/Disk.cs
+++ Day 7/Disk.cs
@@ -1,18 +1,51 @@
 using System.Collections.Generic;
+using System;
 
-namespace Day_7 {
-    class Disk {
-        private string Name;
-        private int weight;
-        private List<Disk> nextDisks;
+namespace Day_7
+{
+    class Disk
+    {
+        public string Name;
+        public int weight;
+        public List<Disk> nextDisks;
+        public List<string> nextDisksString;
+        public Disk parent = null;
 
-        public Disk(string name, int weight) {
+        public Disk(string name, int weight, List<string> list)
+        {
             this.Name = name;
             this.weight = weight;
             this.nextDisks = new List<Disk>();
+            this.nextDisksString = list;
         }
 
-        public Disk() : this(null, 0) {
+        public Disk() : this(null, 0, null)
+        {
+        }
+
+        public Disk getRoot()
+        {
+            return parent != null ? parent.getRoot() : this;
+        }
+
+        public int getWeight()
+        {
+            var w = weight;
+            foreach (var d in nextDisks)
+            {
+                w += d.getWeight();
+            }
+
+            return w;
+        }
+
+        public void checkWeight(int needed)
+        {
+            foreach (var disk in nextDisks)
+            {
+                var x = disk.getWeight();
+                Console.WriteLine(x);
+            }
         }
     }
 }
--- Day 7/Program.cs
+++ Day 7/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace Day_7
 {
@@ -10,24 +11,56 @@ namespace Day_7
 
         private static readonly Regex regex = new Regex(@"([a-z]+) \(([\d]+)\)(?: -> ([a-z, ]+))?");
         private static Disk root = new Disk();
+        private static List<Disk> disks = new List<Disk>();
+        private static Dictionary<string, Disk> dict = new Dictionary<string, Disk>();
 
         static void Main(string[] args)
         {
-            try {
-                using (StreamReader sr = new StreamReader("input.txt")) {
-                   while (!sr.EndOfStream) {
-                       var line = sr.ReadLine();
+            try
+            {
+                using (StreamReader sr = new StreamReader("input.txt"))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        var line = sr.ReadLine();
 
-                       var match = regex.Match(line);
-                       if (match.Success) {
-                           var name = match.Groups[1].Value;
-                           var weight = int.Parse(match.Groups[2].Value);
-                           var nextDisks = match.Groups[3].Value.Split(new char[]{' ',','}, StringSplitOptions.RemoveEmptyEntries);
-                       }
-                   }
+                        var match = regex.Match(line);
+                        if (match.Success)
+                        {
+                            var name = match.Groups[1].Value;
+                            var weight = int.Parse(match.Groups[2].Value);
+                            var nextDisks = match.Groups[3].Value.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                            disks.Add(new Disk(name, weight, new List<string>(nextDisks)));
+                        }
+                    }
+                    disks.ForEach(e =>
+                    {
+                        dict.Add(e.Name, e);
+                    });
+                    root = disks[0];
+
+                    for (int i = disks.Count - 1; i >= 0; i--)
+                    {
+                        var disk = disks[i];
+                        if (disk.nextDisksString.Count > 0)
+                        {
+                            disk.nextDisksString.ForEach(e =>
+                            {
+                                dict[e].parent = disk;
+                                disk.nextDisks.Add(dict[e]);
+                            });
+                        }
+
+                        disks.Remove(disk);
+                    }
+
+                    root = root.getRoot();
+                    
+                    Console.WriteLine(root.Name);
                 }
             }
-            catch (Exception e) {}
+            catch (Exception e) { }
         }
     }
 }
