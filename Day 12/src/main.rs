893dbb99a786812a1ab0f9036df679441a87bd6b
Christoph Schleßelmann <c.schlesselmann@googlemail.com>
2018-09-26 21:47:08 +0200

    Moved input to file

--- Day 12/src/main.rs
+++ Day 12/src/main.rs
@@ -1,12 +1,18 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-fn main() {
+fn get_input() -> String {
     let filename = "input.txt";
     let mut file = File::open(filename).expect("file not found");
 
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("something went wrong reading the file");
+
+    contents
+}
+
+fn main() {
+    let contents = get_input();
 
     let lines: Vec<&str> = contents.split("\n").collect();
     for line in lines {
